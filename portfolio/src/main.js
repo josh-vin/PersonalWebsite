8cf4bbfa03fe5c95bff82b03977fc796bad04613
Josh Vincent <jdvswimmer@gmail.com>
2019-08-04 02:19:04 -0600

    Revert "AWS Amplify Configuration"
    This reverts commit 3f923a8a76ea7a6dde34c12a831533a8cc419e75.

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue';
 import router from "./router";
 import VueSimpleMarkdown from 'vue-simple-markdown'
 import 'vue-simple-markdown/dist/vue-simple-markdown.css'
-import { AmplifyPlugin } from 'aws-amplify-vue'
 
-Vue.use(AmplifyPlugin)
 Vue.use(VueSimpleMarkdown)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 new Vue({
   router,
