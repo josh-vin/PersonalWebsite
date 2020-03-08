c16ec169a954218c7884686fff10d5cf515a8b99
Josh <jdvswimmer@gmail.com>
2020-03-08 17:01:17 -0600

    Fixes google analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import jquery from 'jquery'
 import VueSimpleMarkdown from 'vue-simple-markdown'
 import 'vue-simple-markdown/dist/vue-simple-markdown.css'
-import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueSimpleMarkdown)
 
@@ -20,10 +19,3 @@ require('font-awesome/css/font-awesome.css')
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-Vue.use(VueAnalytics, {
-  id: 'UA-145116461-1',
-  autoTracking: {
-    screenview: true
-  },
-})
