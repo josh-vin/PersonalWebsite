8c3c6f13607b0dc3f825bfbc6fa225e68c2f5cbd
Josh <jdvswimmer@gmail.com>
2020-03-08 21:47:55 -0600

    Fixes the links

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ Vue.prototype.jquery = jquery
 Vue.config.productionTip = false
 
 require('bootstrap')
+require('jquery.easing')
 require('bootstrap/dist/css/bootstrap.css')
 require('devicons/css/devicons.css')
 require('font-awesome/css/font-awesome.css')
